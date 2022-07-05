66de6de8bdfb7561966ada311c4ebda4e0e57564
Spartan859 <36628376+Spartan859@users.noreply.github.com>
2022-07-05 11:47:51 +0800

    Add tensorboard support

--- python_module_SEQ/setup.py
+++ python_module_SEQ/setup.py
@@ -1,6 +1,7 @@
-from setuptools import setup
+import setuptools
 
-setup(name='SEQ',        # Secondary directory
+setuptools.setup(name='SEQ',        # Secondary directory
     version='0.1',
+    packages=setuptools.find_packages(),
     install_requires=['gym','stable-baselines3']    # And any other dependencies foo needs
 )
--- train_ppo.py
+++ train_ppo.py
@@ -5,7 +5,7 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_checker import check_env
 from stable_baselines3.common.evaluation import evaluate_policy
 
-env=gym.make('sequence_mdp-v2',div=1,len=1000,qlen=40) # see python_module_SEQ/SEQ/sequence_mdp_v2.py for variable list
+env=gym.make('sequence_mdp-v2',div=1,len=1000,qlen=40,Rk=0.1) # see python_module_SEQ/SEQ/sequence_mdp_v2.py for variable list
 
 name="PPO_seq_v2_c10"
 len=1000
@@ -15,10 +15,10 @@ train_ep=1000
 
 #'''
 if os.path.exists(name+'.zip'):
-    model=PPO.load(name,env=env,verbose=0)
+    model=PPO.load(name,env=env,verbose=1,tensorboard_log="./PPO_seq_tensorboard/")
     print("Load from" +name+ ".zip")
 else:
-    model=PPO('MlpPolicy',env)
+    model=PPO('MlpPolicy',env,verbose=1,tensorboard_log="./PPO_seq_tensorboard/")
     print("New Start as "+name)
 
 model.learn(train_ep*len)
